3e4376046b123f1c49adac07e2bd5e5185ad0417
Jan Mark Lauron <janmarklauron@gmail.com>
2025-09-25 21:53:31 +0800

    Add project files.

---
+++ FINANCE.TRACKER.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FINANCE.TRACKER.API.Model.Category;
+using Microsoft.EntityFrameworkCore;
+
+namespace FINANCE.TRACKER.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<BudgetCategoryModel> BudgetCategories { get; set; }
+        public DbSet<ExpensesCategoryModel> ExpensesCategories { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ FINANCE.TRACKER.API/Model/Category/BudgetCategoryModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FINANCE.TRACKER.API.Model.Category
+{
+    public class BudgetCategoryModel
+    {
+        [Key]
+        public int BudgetCategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? BudgetCategoryName { get; set; }
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string? BudgetCategoryDescription { get; set; }
+
+        public int IsActive { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
